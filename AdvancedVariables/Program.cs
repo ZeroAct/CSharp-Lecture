2182513e380e6d8a0be789979346962491f0484d
ZeroAct <lhj5682@naver.com>
2023-06-03 15:58:37 +0900

    section 5 ends

---
+++ AdvancedVariables/Program.cs
@@ -0,0 +1,106 @@
+﻿// Array
+
+char[] vowels = new char[5];  // array of 5 chars
+
+vowels[0] = 'a';
+vowels[1] = 'e';
+vowels[2] = 'i';
+vowels[3] = 'o';
+vowels[4] = 'u';
+
+Console.WriteLine(vowels);  // a
+
+foreach (var vowel in vowels)
+{
+    Console.WriteLine(vowel);
+}
+
+for (var i = 1; i <= vowels.Length; i++)
+{
+    Console.WriteLine(vowels[^i]);
+}
+
+Console.WriteLine(vowels[..3]); // aei
+Console.WriteLine(vowels[2..]); // iou
+Console.WriteLine(vowels[1..4]); // eio
+Console.WriteLine(vowels[..]); // aeiou
+
+
+Range range = 1..4;
+Console.WriteLine(vowels[range]); // eio
+
+
+// Two dimensional array
+int[,] matrix = new int[2, 3];  // 2 rows and 3 columns
+
+matrix[0, 0] = 1;
+matrix[0, 1] = 2;
+matrix[0, 2] = 3;
+matrix[1, 0] = 4;
+matrix[1, 1] = 5;
+matrix[1, 2] = 6;
+
+int[,] matrix2 = new int[2, 3] { 
+    { 1, 2, 3 }, 
+    { 4, 5, 6 }
+};
+
+
+for (int row = 0; row < matrix2.GetLength(0); row++)
+{
+    for (int column = 0; column < matrix2.GetLength(1); column++)
+    {
+        Console.Write($"{matrix2[row, column]} ");
+    }
+    Console.WriteLine();
+}
+
+
+// Jagged array
+int[][] matrix3 = new int[2][];  // 2 rows
+
+matrix3[0] = new int[3] { 1, 2, 3 };
+matrix3[1] = new int[2] { 4, 5 };
+
+for (int row = 0; row < matrix3.Length; row++)  // can assign to 'row' because it is a 'for loop variable'
+{
+    for (int column = 0; column < matrix3[row].Length; column++)
+    {
+        Console.Write($"{matrix3[row][column]} ");
+    }
+    Console.WriteLine();
+}
+
+foreach (var row in matrix3)  // cannot assign to 'row' because it is a 'foreach iteration variable'
+{
+    foreach (var column in row)
+    {
+        Console.Write($"{column} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+// String manipulation
+var firstName = "Mosh";
+
+Console.WriteLine(firstName.ToUpper());  // M
+
+if (firstName.Equals("MOSH", StringComparison.InvariantCultureIgnoreCase))
+    Console.WriteLine("Same");
+else
+    Console.WriteLine("Different");
+
+
+var firstNamewithSpaces = " Mosh     ";
+Console.WriteLine(firstNamewithSpaces.Trim());  // Mosh
+
+var indetificationNumber = "123-456-789";
+Console.WriteLine(indetificationNumber.Replace('-', '.'));  // 123.456.789
+
+var amount1 = 123;
+var amount2 = 123.45;
+var amount3 = 123.4567;
+
+Console.WriteLine(amount1.ToString().PadLeft(10, '0'));  // 0000000123
---
+++ Functions/Program.cs
@@ -0,0 +1,238 @@
+﻿// Simple Function
+string GetName()
+{
+    return "Mosh";
+}
+
+var myNmae = GetName();
+
+Console.WriteLine(myNmae);
+
+
+void PrintCurrentDateAndTime()
+{
+    Console.WriteLine(DateTime.Now);
+}
+PrintCurrentDateAndTime();
+
+
+// 
+double Average(int[] nubmers)
+{
+    var sum = 0.0;
+    foreach (var number in nubmers)
+        sum += number;
+
+    return sum / nubmers.Length;
+}
+
+Console.WriteLine(Average(new int[] { 1, 2, 3, 4, 5 }));
+
+// Using parmas
+
+double Average2(params int[] nubmers)
+{
+    var sum = 0.0;
+    foreach (var number in nubmers)
+        sum += number;
+
+    return sum / nubmers.Length;
+}
+
+Console.WriteLine(Average2(1, 2, 3, 4, 5));
+
+double Average3(bool dummy, params int[] nubmers)
+{
+    var sum = 0.0;
+    foreach (var number in nubmers)
+        sum += number;
+
+    return sum / nubmers.Length;
+}
+
+Console.WriteLine(Average3(true, 1, 2, 3, 4, 5));
+
+
+// passing by reference
+void Increment(int number)
+{
+    number += 10;
+}
+
+var number2 = 1;
+Increment(number2);
+Console.WriteLine(number2);  // 1
+
+void Increment2(ref int number)
+{
+    number += 10;
+}
+
+var number3 = 1;
+Increment2(ref number3);
+Console.WriteLine(number3);  // 11
+
+// but array is a reference type
+void Increment3(int[] numbers)
+{
+    numbers[0] += 10;
+}
+
+var numbers2 = new int[] { 1, 2, 3 };
+Increment3(numbers2);
+Console.WriteLine(numbers2[0]);  // 11
+
+
+// out parameter
+int Parse(string number)
+{
+    int result = int.Parse(number);
+    return result;
+}
+
+int result2;
+var success = int.TryParse("abc", out result2);
+if (success)
+    Console.WriteLine(result2);
+else
+    Console.WriteLine("Conversion failed.");
+
+// Using out parameter
+void Doubling(int number, out int result)
+{
+    result = number * 2;
+}
+
+Doubling(10, out int result3);
+Console.WriteLine(result3);  // 20
+
+// Using multiple out parameter
+void Doubling2(int number, out int result, out int result4)
+{
+    result = number * 2;
+    result4 = number * 4;
+}
+
+Doubling2(10, out int result5, out int result6);
+Console.WriteLine(result5);  // 20
+Console.WriteLine(result6);  // 40
+
+
+// Tuple
+var myTuple = (1, 2);
+Console.WriteLine(myTuple.Item1);  // 1
+Console.WriteLine(myTuple.Item2);  // 2
+
+(int, int) myTuple2 = (1, 2);
+Console.WriteLine(myTuple2.Item1);  // 1
+Console.WriteLine(myTuple2.Item2);  // 2
+
+(int a, int b) myTuple3 = (1, 2);
+Console.WriteLine(myTuple3.a);  // 1
+Console.WriteLine(myTuple3.b);  // 2
+
+(int, int) MyTuple()
+{
+    return (1, 2);
+}
+
+var myTuple4 = MyTuple();
+Console.WriteLine(myTuple4.Item1);  // 1
+Console.WriteLine(myTuple4.Item2);  // 2
+
+
+
+// local function
+
+// lambda expression
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+int Sum2(int a, int b) => a + b;
+
+Console.WriteLine(Sum(1, 2));  // 3
+Console.WriteLine(Sum2(1, 2));  // 3
+
+
+
+// Stroing Void Functions in a Variable
+Action printMesage;
+void PrintDateAndTime()
+{
+    Console.WriteLine(DateTime.Now);
+}
+
+void PrintName()
+{
+    Console.WriteLine("Mosh");
+}
+
+printMesage = PrintDateAndTime;
+printMesage();  // 2021-08-22 12:00:00 AM
+
+printMesage = PrintName;
+printMesage();  // Mosh
+
+
+// Passing a function as parameter
+void Process(Action action)
+{
+    Console.WriteLine("Started...");
+    action();
+    Console.WriteLine("Done.");
+}
+
+Process(PrintDateAndTime);
+
+Action<int, int> printSum;
+printSum = (a, b) => Console.WriteLine(a + b);
+printSum(1, 2);  // 3
+
+
+// Func
+Func<string> returnString;
+returnString = () => "Mosh";
+Console.WriteLine(returnString());  // Mosh
+
+Func<int, int, int> sum;  // last int is return type
+sum = (a, b) => a + b;
+Console.WriteLine(sum(1, 2));  // 3
+
+
+// Predicates
+Predicate<string> isUpper;
+isUpper = (text) => text.Equals(text.ToUpper());
+Console.WriteLine(isUpper("hello"));  // False
+
+
+// Delegate
+TryParseDelegate myDelegate = TryParse;
+
+if (myDelegate("123", out int result7))
+    Console.WriteLine(result7);
+else
+    Console.WriteLine("Conversion failed.");
+
+bool TryParse(string value, out int result)
+{
+    return int.TryParse(value, out result);
+}
+
+
+// Anonymous Methods
+Process(() => Console.WriteLine("Hello World"));
+Process(() =>
+{
+    Console.WriteLine("Hello World");
+    Console.WriteLine("Hello World");
+});
+
+delegate bool TryParseDelegate(string value, out int result);
+
+
+// Action is a delegate type that returns void
+// Func is a delegate type that returns a value
+// Predicate is a delegate type that returns a boolean value
+// Delegate is a reference type that can be used to encapsulate a named or an anonymous method
